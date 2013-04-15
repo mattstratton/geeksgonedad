c1ac2f814e43e0b2f6155a0e235ef4ab3316c0ec
Matt Stratton <matt.stratton@gmail.com>
2013-04-15 11:31:49 -0500

    Adjust sidebar widgets to remove bullets

--- content/themes/geeksgonedad-roots/lib/widgets.php
+++ content/themes/geeksgonedad-roots/lib/widgets.php
@@ -7,7 +7,7 @@ function roots_widgets_init() {
   register_sidebar(array(
     'name'          => __('Primary Sidebar', 'roots'),
     'id'            => 'sidebar-primary',
-    'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
+    'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner widget-sidebar">',
     'after_widget'  => '</div></section>',
     'before_title'  => '<h3>',
     'after_title'   => '</h3>',
