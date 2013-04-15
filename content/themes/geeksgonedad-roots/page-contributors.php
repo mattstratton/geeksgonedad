e2a5876d3160b834fb81cf237d6f326a3add7753
Matt Stratton <matt.stratton@gmail.com>
2013-04-15 14:34:05 -0500

    Update formatting on contributor page and move contributor styling to authors.less

--- content/themes/geeksgonedad-roots/page-contributors.php
+++ content/themes/geeksgonedad-roots/page-contributors.php
@@ -3,13 +3,13 @@
 Template Name: Contributors Page Template
 */
 ?>
-<div class="row-fluid">
+<div class="row-fluid author-row">
 <?php $authors = get_users('exclude=1');
 foreach ($authors as $user) {
 	
 	
 ?>
-<div class="author-row span4"> 
+<div class="author-box span4"> 
 		<h1 class="author-name"><?php echo $user->display_name; ?></h1>
 		<?php $avatar = "<img alt='{$safe_alt}' src='{$default}' class='avatar avatar-{$size} photo author-photo' height='{$size}' width='{$size}' />"; 
 			$authorphoto = apply_filters('get_avatar', $avatar, $user->ID,120, $default, $alt);
